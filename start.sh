db1e0c84dac8a88a1a19d8bdc277908059a66188
alix <chehome@mail.ru>
2019-12-17 16:42:44 +0300

    added some

--- start.sh
+++ start.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
-#MariaDB variables to set
-#mdbip=10.88.0.200
-#Oned vars
-#onedip=10.88.0.201
-#onedport=2633
-#nginx
-#nginxip=10.88.0.202
-#nginxport=80
-#novncport=29876
-#Onegate vars
-#gateip=10.88.0.203
-#gateport=5030
-#Oneflow vars
-#flowip=10.88.0.204
-
+read -p 'pods data folder? (default=/data): ' deffold
+if [ -z $deffold ]
+then deffold=/data
+fi
 read -p 'mariadb img name? (default=mdb): ' mdbnm
 if [ -z $mdbnm ]
 then mdbnm=mdb
@@ -26,13 +15,13 @@ read -p 'mariadb oneadmin passwd? (default=passwd): ' mdbusr
 if [ -z $mdbusr ]
 then mdbusr=passwd
 fi
-read -p 'mariadb db volume? (default=/data/mysql): ' mdbvol
+read -p 'mariadb db volume? (default=mysql): ' mdbvol
 if [ -z $mdbvol ]
-then mdbvol=/data/mysql
+then mdbvol=mysql
 fi
-read -p 'log volume? (default=/data/mysql): ' logvol
+read -p 'log volume? (default=log): ' logvol
 if [ -z $logvol ]
-then logvol=/data/log
+then logvol=log
 fi
 read -p 'base img name? (default=baseimg): ' basenm
 if [ -z $basenm ]
@@ -58,29 +47,29 @@ read -p 'gate img name? (default=gate): ' gatenm
 if [ -z $gatenm ]
 then gatenm=gate
 fi
-read -p 'volume for etc files (default=/data/opennebula/etc)' etcfiles
+read -p 'volume for etc files (default=etc)' etcfiles
 if [ -z $etcfiles ]
-then etcfiles="/data/opennebula/etc"
+then etcfiles="etc"
 fi
-read -p 'volume for var files (default=/data/opennebula/var)' varfiles
+read -p 'volume for var files (default=var)' varfiles
 if [ -z $varfiles ]
-then varfiles="/data/opennebula/var"
+then varfiles="var"
 fi
-read -p "pod\'s name (default=onepod)" podsnm
+read -p "pod's name (default=onepod)" podsnm
 if [ -z $podsnm ]
 then podsnm="onepod"
 fi
-read -p "pod\'s publised to the host web port (default=8080)" podwport
+read -p "pod's publised to the host web port (default=8080)" podwport
 if [ -z $podwport ]
 then podwport="8080"
 fi
-read -p "pod\'s publushed to the host noVNC port (default=29876)" podvncport
+read -p "pod's publushed to the host noVNC port (default=29876)" podvncport
 if [ -z $podvncport ]
 then podvncport="29876"
 fi
 
 fullbasenm=localhost/"$basenm"
-
+currpath=$(/bin/pwd)
 cd mariadb
 podman build -t "$mdbnm" .
 cd ../baseimg
@@ -96,15 +85,24 @@ podman build --build-arg image="$fullbasenm" -t "$flownm" .
 cd ../gate
 podman build --build-arg image="$fullbasenm" -t "$gatenm" .
 cd ../
+
+mkdir -p -m 777 "$deffold" "$deffold"/"$logvol"
+mkdir -p "$deffold"/"$varfiles"/.one && echo "oneadmin:Sonic2005" > "$deffold"/"$varfiles"/.one/one_auth
+mkdir -p "$deffold"/"$etcfiles" && chown -R 9869:9869 "$deffold"/"$etcfiles" "$deffold"/"$varfiles"
+mkdir -p "$deffold"/"$mdbvol" && chown -R 27:27 "$deffold"/"$mdbvol"
+cd "$deffold"/"$etcfiles" && tar -xvf $currpath/etc_v10.tar
+cd "$deffold"/"$varfiles" && tar -xvf $currpath/var_v10.tar
+
 podman pod create --name $podsnm --publish "$podwport":80 --publish "$podvncport":29876
-podman run -dt --pod $podsnm --name=mdb -e MYSQL_ROOT_PASSWORD="$mdbroot" -e MYSQL_USER=oneadmin -e MYSQL_PASSWORD="$mdbusr" -e MYSQL_DATABASE=opennebula  -v "$mdbvol":/var/lib/mysql -v "$logvol":/var/log/mariadb "$mdbnm"
+podman run -dt --pod $podsnm --name=mdb -e MYSQL_ROOT_PASSWORD="$mdbroot" -e MYSQL_USER=oneadmin -e MYSQL_PASSWORD="$mdbusr" -e MYSQL_DATABASE=opennebula  -v "$deffold"/"$mdbvol":/var/lib/mysql -v "$deffold"/"$logvol":/var/log/mariadb "$mdbnm"
 sleep 5
-podman run -dt --pod $podsnm --name=onedpod -v "$etcfiles":/etc/one  -v "$varfiles":/var/lib/one -v "$logvol":/var/log/one "$onednm"
+podman run -dt --pod $podsnm --name=onedpod -v "$deffold"/"$etcfiles":/etc/one  -v "$deffold"/"$varfiles":/var/lib/one -v "$deffold"/"$logvol":/var/log/one "$onednm"
 sleep 5
-podman run -dt --pod $podsnm --name=schedpod -v "$etcfiles":/etc/one  -v "$varfiles":/var/lib/one -v "$logvol":/var/log/one "$schednm"
-podman run -dt --pod $podsnm --name=nginxpod -v "$etcfiles":/etc/one  -v "$varfiles":/var/lib/one -v "$logvol":/var/log/one "$nginxnm"
-podman run -dt --pod $podsnm --name=gatepod -v "$etcfiles":/etc/one  -v "$varfiles":/var/lib/one -v "$logvol":/var/log/one "$gatenm"
-podman run -dt --pod $podsnm --name=flowpod -v "$etcfiles":/etc/one  -v "$varfiles":/var/lib/one -v "$logvol":/var/log/one "$flownm"
+podman run -dt --pod $podsnm --name=schedpod -v "$deffold"/"$etcfiles":/etc/one  -v "$deffold"/"$varfiles":/var/lib/one -v "$deffold"/"$logvol":/var/log/one "$schednm"
+podman run -dt --pod $podsnm --name=nginxpod -v "$deffold"/"$etcfiles":/etc/one  -v "$deffold"/"$varfiles":/var/lib/one -v "$deffold"/"$logvol":/var/log/one "$nginxnm"
+podman run -dt --pod $podsnm --name=gatepod -v "$deffold"/"$etcfiles":/etc/one  -v "$deffold"/"$varfiles":/var/lib/one -v "$deffold"/"$logvol":/var/log/one "$gatenm"
+podman run -dt --pod $podsnm --name=flowpod -v "$deffold"/"$etcfiles":/etc/one  -v "$deffold"/"$varfiles":/var/lib/one -v "$deffold"/"$logvol":/var/log/one "$flownm"
+
 #podman run -dt --ip="$mdbip" --name=mariadb -e MYSQL_ROOT_PASSWORD="$mdbroot" -e MYSQL_USER=oneadmin -e MYSQL_PASSWORD="$mdbusr" -e MYSQL_DATABASE=opennebula  -v "$mdbvol":/var/lib/mysql -p $mdbport:3306 "$mdbnm"
 #sleep 5
 #podman run -dt --ip="$onedip" --name=oned -v "$logfiles":/var/log/one -v "$etcfiles":/etc/one  -v "$varfiles":/var/lib/one -p $onedport:2633 "$onednm"
