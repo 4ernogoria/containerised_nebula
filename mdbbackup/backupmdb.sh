f85bdc652673e1652de73ec5e5d72b62bfa2f761
alix <chehome@mail.uu>
2019-12-20 15:02:11 +0500

    added a mysql container

---
+++ mdbbackup/backupmdb.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BACKUP_FOLDER=/opt/mysql/backup
+NOW=$(date '+%d%m%Y')
+
+GZIP=$(which gzip)
+MYSQLDUMP=$(which mysqldump)
+
+### MySQL Server Login info ###
+MHOST=127.0.0.1
+MPASS=$mdbusr
+MUSER=oneadmin
+
+[ ! -d $BACKUP_FOLDER ] && mkdir -p $BACKUP_FOLDER
+
+FILE=$BACKUP_FOLDER/backup-$NOW.sql.gz
+$MYSQLDUMP -h $MHOST -u $MUSER --databases opennebula | $GZIP -9 > $FILE
