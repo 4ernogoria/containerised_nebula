7d774f29fa2f9d24c3e93f87132871e40d959138
alix <chehome@mail.uu>
2019-12-20 19:18:45 +0500

    added the  backup files cleaning

--- mdbbackup/backupmdb.sh
+++ mdbbackup/backupmdb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 BACKUP_FOLDER=/opt/mysql/backup
+BACKUP_FOLDER_D=/opt/mysql/backup/daily
+BACKUP_FOLDER_W=/opt/mysql/backup/weekly
 NOW=$(date '+%d%m%Y')
 
 GZIP=$(which gzip)
@@ -12,6 +14,8 @@ MPASS=$mdbusr
 MUSER=oneadmin
 
 [ ! -d $BACKUP_FOLDER ] && mkdir -p $BACKUP_FOLDER
+[ ! -d $BACKUP_FOLDER_D ] && mkdir -p $BACKUP_FOLDER_D
+[ ! -d $BACKUP_FOLDER_W ] && mkdir -p $BACKUP_FOLDER_W
 
 FILE=$BACKUP_FOLDER/backup-$NOW.sql.gz
 $MYSQLDUMP -h $MHOST -u $MUSER --log-error=/var/log/mysql --databases opennebula | $GZIP -9 > $FILE
