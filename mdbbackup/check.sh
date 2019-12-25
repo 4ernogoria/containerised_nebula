d480bbf864bd17013d00d5e36c2a8b35bdaa46f4
root <root@ovirt1.example.com>
2019-12-25 19:42:14 +0500

    changes backup container added tzdata package and set TZs to check.sh

--- mdbbackup/check.sh
+++ mdbbackup/check.sh
@@ -2,17 +2,17 @@
 rm /var/log/mariadb/check.file  2&>/dev/null
 if [ $? ]
 then
-echo "$(date '+%H:%M:%S %d:%m:%Y') check.file was removed sucessfully during the container's start"  >> /var/log/mariadb/checker.log
+echo "$(TZ='Europe/Moscow' date '+%H:%M:%S %d:%m:%Y') check.file was removed sucessfully during the container's start"  >> /var/log/mariadb/checker.log
 else
-echo "$(date '+%H:%M:%S %d:%m:%Y') check.file wasn't removed during the container's start" >> /var/log/mariadb/checker.log
+echo "$(TZ='Europe/Moscow' date '+%H:%M:%S %d:%m:%Y') check.file wasn't removed during the container's start" >> /var/log/mariadb/checker.log
 fi
 filecheck=$(ls /opt/var/.one | grep -v one_auth | wc -l)
 mysqlcheck=$(mysql -sNe "select count(table_name) from INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='opennebula';")
 if [ "$filecheck" -gt 1 -a $mysqlcheck -gt 0 ]
 then
-	echo "$(date '+%H:%M:%S %d:%m:%Y') the data already has taken its place" >> /var/log/mariadb/checker.log
+	echo "$(TZ='Europe/Moscow' date '+%H:%M:%S %d:%m:%Y') the data already has taken its place" >> /var/log/mariadb/checker.log
 else
-	echo "$(date '+%H:%M:%S %d:%m:%Y') previous nebula data wasn't found, createing check.file (which trigers /etc/one/* /var/lib/one/* overwritting ) " >> /var/log/mariadb/checker.log
+	echo "$(TZ='Europe/Moscow' date '+%H:%M:%S %d:%m:%Y') previous nebula data wasn't found, createing check.file (which trigers /etc/one/* /var/lib/one/* overwritting ) " >> /var/log/mariadb/checker.log
 	touch /var/log/mariadb/check.file
 fi
 tail -f /var/log/mariadb/checker.log
