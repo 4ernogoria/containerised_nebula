c74e480d4ae19b0b88eb224f4c2e46296e617dc4
4ernogoria <chehome@mail.ru>
2020-01-21 12:25:16 +0500

    changed the backup mechanism

--- mdbbackup_init/cleanerd.sh
+++ mdbbackup_init/cleanerd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-folder_d=/opt/mysql/backup
+BACKUP_FOLDER=/opt/mysql/backup/daily
+export $BACKUP_FOLDER
+
 filename=$(find $folder_d -type f | sort -n | head -n 1)
 echo $filename
-mv "$filename" $folder_d/daily/
+cp "$filename" $folder_d/daily/
 rm -f $folder_d/* 2&>/dev/null
 
